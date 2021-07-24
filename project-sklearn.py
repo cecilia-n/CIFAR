b5da073b1008b295a1718a156a77fc7332896caa
theta291 <theta291@gmail.com>
2021-07-24 15:55:56 -0700

    Lmao we accidentally didn't drop the labels. Fixed it.

--- project-sklearn.py
+++ project-sklearn.py
@@ -18,10 +18,10 @@ allTestData = pd.read_csv('./test.csv')
 # allTrainingData = allTrainingData.drop(labels="label", axis=1)
 X_train, X_test = train_test_split(allTrainingData, test_size=0.3, random_state=1)
 y_train = X_train["label"]
-X_train.drop(labels="label", axis=1)
+X_train.drop(labels="label", axis=1, inplace=True)
 
 y_test = X_test["label"]
-X_test.drop(labels="label", axis=1)
+X_test.drop(labels="label", axis=1, inplace=True)
 
 scaler = MinMaxScaler()
 scaler.fit(X_train)
