64d3d18ccbd2e226d77b2ba44c7025ec80f2ad3f
theta291 <theta291@gmail.com>
2021-07-26 00:04:17 -0700

    Added some comments on preprocessing

--- project_preprocessing.py
+++ project_preprocessing.py
@@ -3,9 +3,11 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 
+# Importing data
 allTrainingData = pd.read_csv('./train.csv')
 allTestData = pd.read_csv('./test.csv')
 
+# Splitting and dropping labels
 X_train, X_test = train_test_split(allTrainingData, test_size=0.3, random_state=1)
 y_train = X_train["label"]
 X_train.drop(labels="label", axis=1, inplace=True)
@@ -13,11 +15,13 @@ X_train.drop(labels="label", axis=1, inplace=True)
 y_test = X_test["label"]
 X_test.drop(labels="label", axis=1, inplace=True)
 
+# Scaling
 scaler = MinMaxScaler()
 scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
+# One-hot encoding
 encoder = OneHotEncoder()
 encoder.fit(y_train.values.reshape(-1, 1))
 y_train = encoder.transform(y_train.values.reshape(-1, 1)).toarray()
