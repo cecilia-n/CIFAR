7539e5bf6e34185aa5bdb9c38a9ab9d3228c5a24
theta291 <theta291@gmail.com>
2021-07-25 22:59:11 -0700

    Finalied first layer imaging code I used this code for the images generated for the discussion.

--- project_firstlayer_imaging.py
+++ project_firstlayer_imaging.py
@@ -9,16 +9,26 @@ first_coefs = mlp.coefs_[0].T
 
 first_layer_size = len(first_coefs)
 
-fig, axs = plt.subplots(ceil(len(first_coefs)/10), 10, figsize=(28, 28))
+# fig, axs = plt.subplots(ceil(len(first_coefs)/10), 10, figsize=(28, 28))
 scaler = MinMaxScaler()
 
-if first_layer_size > 10:
+width = 10
+split = 100
+
+if first_layer_size > width:
     for num, coefs in enumerate(first_coefs):
+        new_num = num % split
+        if not new_num:
+            if num:
+                plt.show()
+            input("Press ENTER to continue")
+            fig, axs = plt.subplots(ceil(split/width), width, figsize=(28, 28))
         new_coefs = scaler.fit_transform(coefs.reshape(-1, 1))
-        axs[divmod(num, 10)].imshow(new_coefs.reshape(28, 28), cmap='gray')
+        axs[divmod(new_num, width)].imshow(new_coefs.reshape(28, 28), cmap='gray')
+    plt.show()
 else:
+    fig, axs = plt.subplots(first_layer_size, figsize=(28, 28))
     for num, coefs in enumerate(first_coefs):
         new_coefs = scaler.fit_transform(coefs.reshape(-1, 1))
         axs[num].imshow(new_coefs.reshape(28, 28), cmap='gray')
-
-plt.show()
+    plt.show()
