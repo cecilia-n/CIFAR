05a96ce722efe800dd6bac40a0d320ca41733615
theta291 <theta291@gmail.com>
2021-07-25 23:45:52 -0700

    Added average image documentation Added documentation for how to get the average images.

--- project_average_digit_imaging.py
+++ project_average_digit_imaging.py
@@ -4,12 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def brute_sum(thing, start=0):
+    '''
+    Function that adds all the elements of an iterable to the start value.
+
+    parameters:
+    thing: any iterable
+    start: the value to which everything will be added
+
+    returns:
+    the sum
+    '''
     for elem in thing:
         start += elem
     return start
 
+# A script that plots all the averages.
 fig, axs = plt.subplots(2, 5, figsize=(28, 28))
-
 for digit, frequency in allTrainingData['label'].value_counts().iteritems():
     inds = allTrainingData['label'] == digit
     average = brute_sum(allTrainingData[inds].drop(columns='label').to_numpy(), start=np.zeros(784)) / frequency
